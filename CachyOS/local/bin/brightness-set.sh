093cb33a87f751b1903894ee553377b002e81a41
fkeskici <fkeskici@gmail.com>
2025-06-21 15:33:01 +0300

    update

--- CachyOS/local/bin/brightness-set.sh
+++ CachyOS/local/bin/brightness-set.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-[ "$1" = 10 ] && percent="1" || percent="0.$1"
-
 # Parlaklık ayarı
-brightnessctl set "$1%"
+if ! brightnessctl set "$1%"; then
 
-# Bildirim
+# Hata durumunda bildirim gönder
+notify-send -t 5000 -u critical "Parlaklık ayarlanırken bir hata oluştu!" "Lütfen 'brightnessctl' komutunun doğru çalıştığından ve izinlerinizin olduğundan emin olun."
+
+else
+
+# Başarılı bildirim
 notify-send -t 3000 -h string:bgcolor:#ebcb8b "Parlaklık %$1 ayarlandı."
+
+fi
